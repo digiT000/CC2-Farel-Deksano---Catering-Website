13e08b37f94841f246aa64421d226f7a997d2358
Farel Darari Deksano <65439249+digiT000@users.noreply.github.com>
2025-02-07 00:48:44 +0700

    Update api.ts
    Remove Console

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,7 +6,6 @@ export async function fetchDataProduct() {
   const response = await axios.get(
     `/spaces/2gchvvxde1g0/environments/master/entries?access_token=${ACCESS_TOKEN}`
   );
-  console.log(response);
   return response.data;
 }
 
